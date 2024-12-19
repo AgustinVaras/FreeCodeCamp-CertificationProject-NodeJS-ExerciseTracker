6c5a94e1684ff49efd51465a63bac52ccbf2e9da
Agustin Varas <Agustin.Varas@outlook.com.ar>
2024-12-19 13:57:53 +0000

    feat: Connected to MongoDB succesfully

--- index.js
+++ index.js
@@ -3,6 +3,13 @@ const app = express()
 const cors = require('cors')
 require('dotenv').config()
 
+//MongoDB connection
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log('MongoDB connected succesfully'))
+  .catch((err) => console.error('Error connecting to DB: ' + err));
+//-----------------------------------------------------------
 app.use(cors())
 app.use(express.static('public'))
 app.get('/', (req, res) => {
